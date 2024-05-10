5abd74cf1c86132ce690e8031878beaaabcf3cb2
YoungNavigator <andriej2301@gmail.com>
2024-05-10 13:48:59 +0200

    feature: Logging errors with database connection

--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.EntityFrameworkCore;
 
 using bankingApplication_API.Data;
 using bankingApplication_API.Interfaces;
 using bankingApplication_API.Repository;
 
-using Microsoft.AspNetCore.Http.Features;
-
 // WebApplication - a class representing the main application object.
 // It aggregates various components and functions that are frequently used in a web application configuration.
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+var loggerFactory = LoggerFactory.Create(builder => builder.AddFile("Logs/log_{Date}.txt"));
+var _logger = loggerFactory.CreateLogger<Program>();
 
 // Add services to the container.
 builder.Services.AddCors(options =>
@@ -39,10 +41,21 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var connectionString = "Data Source=(local);Initial Catalog=Bank;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;MultiSubnetFailover=False\r\n";
-builder.Services.AddDbContext<DataContext>(options =>
+
+try
 {
-    options.UseSqlServer(connectionString);
-});
+    builder.Services.AddDbContext<DataContext>(options =>
+    {
+        options.UseSqlServer(connectionString);
+    });
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occured while connecting to the database: " + ex.Message);
+    _logger.LogError(ex.Message);
+    loggerFactory.Dispose();
+    Environment.Exit(1);
+}
 
 builder.Services.Configure<FormOptions>(options =>
 {
@@ -84,14 +97,24 @@ using (var scope = app.Services.CreateScope())
     var serviceProvider = scope.ServiceProvider;
     var dbContext = serviceProvider.GetRequiredService<DataContext>();
 
-    // Use reflection to get DbSet properties from the DbContext
-    var dbSetProperties = dbContext.GetType().GetProperties()
-        .Where(p => p.PropertyType.IsGenericType && p.PropertyType.GetGenericTypeDefinition() == typeof(DbSet<>));
-
-    foreach (var property in dbSetProperties)
+    try
     {
-        var entityType = property.PropertyType.GetGenericArguments().First();
-        var tableName = dbContext.Model.FindEntityType(entityType).GetTableName();
+        // Use reflection to get DbSet properties from the DbContext
+        var dbSetProperties = dbContext.GetType().GetProperties()
+            .Where(p => p.PropertyType.IsGenericType && p.PropertyType.GetGenericTypeDefinition() == typeof(DbSet<>));
+
+        foreach (var property in dbSetProperties)
+        {
+            var entityType = property.PropertyType.GetGenericArguments().First();
+            var tableName = dbContext.Model.FindEntityType(entityType).GetTableName();
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("An error occured while processing database query: " + ex.Message);
+        _logger.LogError(ex.Message);
+        loggerFactory.Dispose();
+        Environment.Exit(1);
     }
 }
 
